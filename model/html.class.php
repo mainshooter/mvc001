5b6fc130d453c29915af066c16c3e4b0f6574d8c
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-31 10:41:41 +0200

    Recreate the create seleectbox method

--- model/html.class.php
+++ model/html.class.php
@@ -3,29 +3,16 @@
   class html {
 
     /**
-     * Creates fotr the selectBox the option
-     * @param  [array] $selectArray [With the key and the value in it]
-     * @return [string]              [Returns a option]
+     * Create selectbox with the posibility to highlight a select option when the
+     * columNames[0] == $highlateID/columNames[2]
+     * @param  [assoc array] $contacts [the result from the db]
+     * @param  [array] $columNames [All the columNames includeing the highlatedID]
+     * @return [string] [With the HTML selectBox]
      */
-    public function createSelectbox($contacts, $columNames) {
-      $selectBox = "<select onchange='contact.mailAdressSelect(this.value)'>";
-      $selectBox .= '<option></option>';
-      foreach ($contacts as $key => $value) {
-        $selectBox .= '<option value="' . $value[$columNames[0]] . '">' . $value[$columNames[1]] . '</option>';
-      }
-      $selectBox .= "</select>";
-      return ($selectBox);
-    }
+    public function createSelectBox($contacts, $columNames) {
+      $highlateID = ISSET($columNames[2])?$columNames[2]: NULL;
 
-    /**
-     * Create a select selectBOX
-     * @param  [assoc array] $contacts   [The result from the db]
-     * @param  [array] $columNames [With all colum names started with the ID]
-     * @param  [INT] $highlateID [The ID of the colum that we wanted to highlight]
-     * @return [string]             [Returns the creates selectBOX]
-     */
-    public function createSelectedSelectbox($contacts, $columNames, $highlateID) {
-      $selectBox = "<select>";
+      $selectBox = "<select onchange='contact.mailAdressSelect(this.value)'>";
       foreach ($contacts as $key => $value) {
         if ($value[$columNames[0]] == $highlateID) {
           $selectBox .= '<option value="' . $value[$columNames[0]] . '" selected>' . $value[$columNames[1]] . '</option>';
@@ -34,12 +21,10 @@
         else {
           $selectBox .= '<option value="' . $value[$columNames[0]] . '">' . $value[$columNames[1]] . '</option>';
         }
-
-      }
-      $selectBox .= "</select>";
-      return($selectBox);
     }
+    $selectBox .= "</select>";
+    return($selectBox);
   }
-
+}
 
 ?>
