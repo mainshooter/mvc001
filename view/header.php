32a211e0b1347f3867e5b7c15c3dc05a15347f15
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-31 09:26:36 +0200

    Added JS

--- view/header.php
+++ view/header.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MVC</title>
     <link rel="stylesheet" href="style/grid.css" type="text/css">
+    <script src="js/main.js"></script>
   </head>
 <body>
 
